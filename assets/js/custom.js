a9261a12d0741df4399d9c1b578fa9844a691ea1
elhayfar <74500613+elhayfar@users.noreply.github.com>
2021-08-23 11:43:18 -0700

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,1 +1,24 @@
 // Custom scripts
+
+$(document).ready(function(){
+  $('.customer-logos').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    arrows: false,
+    dots: false,
+    pauseOnHover: false,
+    responsive: [{
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 4
+      }
+    }, {
+      breakpoint: 520,
+      settings: {
+        slidesToShow: 3
+      }
+    }]
+  });
+});
