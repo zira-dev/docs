bec1664995e1711c6425bb9b32ce114eeaa2bd4f
elhayfar <74500613+elhayfar@users.noreply.github.com>
2021-08-23 20:39:57 -0700

    Update carousel.js

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
   $('.customer-logos').slick({
-    slidesToShow: 6,
+    slidesToShow: 8,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 1000,
+    autoplaySpeed: 500,
     arrows: false,
     dots: false,
     pauseOnHover: false,
