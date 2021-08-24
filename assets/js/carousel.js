14daf4d10ab2d4abe4ca41ef79ea517a785da0d2
elhayfar <74500613+elhayfar@users.noreply.github.com>
2021-08-23 20:49:07 -0700

    Update carousel.js

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     slidesToShow: 6,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 10,
+    autoplaySpeed: 1000,
     arrows: false,
     dots: false,
     pauseOnHover: false,
