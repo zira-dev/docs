124d775fe8c0c13707c2fbeb891eef760b3911c8
elhayfar <74500613+elhayfar@users.noreply.github.com>
2021-08-23 17:04:12 -0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -56,7 +56,7 @@
     function findIndex(array, predicate) {
         return findIndexFn.call(array, predicate);
     }
-    function(){
+    function customer-logos(){
   $('.customer-logos').slick({
     slidesToShow: 6,
     slidesToScroll: 1,
